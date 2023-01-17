84b872c38aedfb213c6a963c5fee06721ca94a1e
Nima Prmdi <nimaprmdi@gmail.com>
2023-01-17 16:04:22 +0330

    Fix minor issues

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import fs from "fs/promises";
 import path from "path";
-import { BackupOutlined } from "@mui/icons-material";
+import Link from "next/link";
 
 const HomePage = (props) => {
     const { products } = props;
@@ -9,22 +9,40 @@ const HomePage = (props) => {
     return (
         <ul>
             {products.map((product) => (
-                <li key={product.id}>{product.title}</li>
+                <li key={product.id}>
+                    <Link href={`/${product.id}`}>{product.title}</Link>
+                </li>
             ))}
         </ul>
     );
 };
 
 export async function getStaticProps() {
+    console.log("Re-Generating");
     // cwd is current working directory
     const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
     const jsonData = await fs.readFile(filePath);
     const data = JSON.parse(jsonData);
 
+    if (!data) {
+        return {
+            redirect: {
+                destination: "/no-project",
+            },
+        };
+    }
+
+    if (data.products.length === 0) {
+        return {
+            notFound: true,
+        };
+    }
+
     return {
         props: {
             products: data.products,
         },
+        revalidate: 10,
     };
 }
 
